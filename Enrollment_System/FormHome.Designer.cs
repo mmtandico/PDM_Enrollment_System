857dd2f41d99318df323ac5346e49eb646b70455
mmtandico <mmtandico@gmail.com>
2025-02-22 15:38:36 +0800

    UPDATE BUTTON

--- Enrollment_System/FormHome.Designer.cs
+++ Enrollment_System/FormHome.Designer.cs
@@ -43,7 +43,10 @@
             // button1
             // 
             this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(253, 15);
             this.button1.Name = "button1";
@@ -54,7 +57,8 @@
             // 
             // button3
             // 
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.Location = new System.Drawing.Point(368, 15);
             this.button3.Name = "button3";
@@ -65,7 +69,8 @@
             // 
             // button4
             // 
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.ForeColor = System.Drawing.Color.White;
             this.button4.Location = new System.Drawing.Point(481, 15);
             this.button4.Name = "button4";
@@ -76,11 +81,12 @@
             // 
             // button2
             // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.Location = new System.Drawing.Point(598, 15);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(89, 41);
+            this.button2.Size = new System.Drawing.Size(124, 41);
             this.button2.TabIndex = 2;
             this.button2.Text = "PERSONAL INFORMATION";
             this.button2.UseVisualStyleBackColor = true;
@@ -98,7 +104,7 @@
             // 
             this.BtnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.BtnLogout.ForeColor = System.Drawing.Color.White;
-            this.BtnLogout.Location = new System.Drawing.Point(817, 22);
+            this.BtnLogout.Location = new System.Drawing.Point(811, 15);
             this.BtnLogout.Name = "BtnLogout";
             this.BtnLogout.Size = new System.Drawing.Size(79, 39);
             this.BtnLogout.TabIndex = 5;
